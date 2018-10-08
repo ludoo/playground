268a8ffd6fadf0170e182607e90c55743f62db53
Ludovico Magnocavallo <ludomagno@google.com>
2018-10-08 15:29:49 +0200

    add support for multiple instances to hetzner instance module

--- tf-modules/hetzner/coreos-instance/main.tf
+++ tf-modules/hetzner/coreos-instance/main.tf
@@ -4,17 +4,20 @@
 # https://gist.github.com/thetechnick/12b33e4edfc96e4ccc9800afef2be7c5
 
 data "template_file" "cloud-config" {
+  count    = "${var.num_instances}"
   template = "${file("${path.module}/cloud-config.yaml")}"
 
   vars = {
-    name         = "${var.name}"
+    name         = "${element(hcloud_server.server.*.name, count.index)}"
     ssh_key      = "${file(var.ssh_public_key_file)}"
-    ipv4_address = "${hcloud_server.server.ipv4_address}"
+    ipv4_address = "${element(hcloud_server.server.*.ipv4_address, count.index)}"
   }
 }
 
 resource "hcloud_server" "server" {
-  name        = "${var.name}"
+  count = "${var.num_instances}"
+
+  name        = "${var.name}${var.num_instances == 1 ? "" : format("-%02d", count.index)}"
   image       = "${var.source_image == "" ? "debian-9" : var.source_image}"
   server_type = "${var.type}"
   ssh_keys    = ["${var.ssh_public_key_id}"]
@@ -23,22 +26,22 @@ resource "hcloud_server" "server" {
 }
 
 resource "null_resource" "coreos-install-from-scratch" {
-  count      = "${var.source_image == "" ? 1 : 0}"
+  count      = "${var.source_image == "" ? var.num_instances : 0}"
   depends_on = ["hcloud_server.server"]
 
   triggers {
-    server_ip = "${hcloud_server.server.ipv4_address}"
+    server_ip = "${element(hcloud_server.server.*.ipv4_address, count.index)}"
   }
 
   connection {
-    host        = "${hcloud_server.server.ipv4_address}"
-    timeout     = "1m"
+    host        = "${element(hcloud_server.server.*.ipv4_address, count.index)}"
+    timeout     = "2m"
     agent       = false
     private_key = "${file(var.ssh_private_key_file)}"
   }
 
   provisioner "file" {
-    content     = "${data.template_file.cloud-config.rendered}"
+    content     = "${element(data.template_file.cloud-config.*.rendered, count.index)}"
     destination = "/root/cloud-config.yaml"
   }
 
@@ -48,23 +51,23 @@ resource "null_resource" "coreos-install-from-scratch" {
 }
 
 resource "null_resource" "coreos-install-from-image" {
-  count      = "${var.source_image == "" ? 0 : 1}"
+  count      = "${var.source_image == "" ? 0 : var.num_instances}"
   depends_on = ["hcloud_server.server"]
 
   triggers {
-    server_ip = "${hcloud_server.server.ipv4_address}"
+    server_ip = "${element(hcloud_server.server.*.ipv4_address, count.index)}"
   }
 
   connection {
-    host        = "${hcloud_server.server.ipv4_address}"
-    timeout     = "1m"
+    host        = "${element(hcloud_server.server.*.ipv4_address, count.index)}"
+    timeout     = "2m"
     agent       = false
     private_key = "${file(var.ssh_private_key_file)}"
     user        = "core"
   }
 
   provisioner "file" {
-    content     = "${data.template_file.cloud-config.rendered}"
+    content     = "${element(data.template_file.cloud-config.*.rendered, count.index)}"
     destination = "/home/core/cloud-config.yaml"
   }
 
@@ -73,6 +76,6 @@ resource "null_resource" "coreos-install-from-image" {
   }
 
   provisioner "local-exec" {
-    command = "ssh -o 'StrictHostKeyChecking no' -i ${var.ssh_private_key_file} core@${hcloud_server.server.ipv4_address} 'sudo bash -c \"(sleep 2 && reboot)&\"'"
+    command = "ssh -o 'StrictHostKeyChecking no' -i ${var.ssh_private_key_file} core@${element(hcloud_server.server.*.ipv4_address, count.index)} 'sudo bash -c \"(sleep 3 && reboot)&\"'"
   }
 }
--- tf-modules/hetzner/coreos-instance/vars.tf
+++ tf-modules/hetzner/coreos-instance/vars.tf
@@ -10,24 +10,29 @@ variable "ssh_public_key_id" {
 }
 
 variable "ssh_public_key_file" {
-  description = "Actual SSH public key matching the id on Hetzner Cloud"
+  description = "Actual SSH public key matching the id on Hetzner Cloud."
 }
 
 variable "ssh_private_key_file" {
-  description = "Private SSH key matching the public key on Hetzner Cloud"
+  description = "Private SSH key matching the public key on Hetzner Cloud."
 }
 
 variable "datacenter" {
-  description = "Server datacenter"
+  description = "Server datacenter."
   default     = "fsn1-dc8"
 }
 
 variable "type" {
-  description = "Server type"
+  description = "Server type."
   default     = "cx11"
 }
 
 variable "source_image" {
   description = "CoreOS snapshot to use for faster instance creation."
   default     = ""
+}
+
+variable "num_instances" {
+  description = "Number of instances to create."
+  default     = 1
 }
