6205cbfba456cdd3c8feb369f3f2527287027ba4
Ludovico Magnocavallo <ludomagno@google.com>
2018-10-08 15:41:51 +0200

    fix hetzner instance module outputs

--- tf-modules/hetzner/coreos-instance/outputs.tf
+++ tf-modules/hetzner/coreos-instance/outputs.tf
@@ -1,14 +1,16 @@
 # Copyright 2018 Google LLC.
 # SPDX-License-Identifier: Apache-2.0
 
-output "id" {
-  value = "${hcloud_server.server.id}"
+resource "null_resource" "server-list" {
+  count = "${length(var.num_instances)}"
+
+  triggers {
+    id           = "${element(hcloud_server.server.*.id, count.index)}"
+    name         = "${element(hcloud_server.server.*.name, count.index)}"
+    ipv4_address = "${element(hcloud_server.server.*.ipv4_address, count.index)}"
+  }
 }
 
-output "name" {
-  value = "${hcloud_server.server.name}"
-}
-
-output "ipv4_address" {
-  value = "${hcloud_server.server.ipv4_address}"
+output "servers" {
+  value = ["${null_resource.server-list.triggers}"]
 }
